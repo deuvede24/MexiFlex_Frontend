831ea7f0a96e2d6b7d3048c3c3e2d9588169875d
deuvede24 <cdaniela.vd@gmail.com>
2024-11-18 00:55:34 +0100

    vercel ch3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-//import { provideToastr } from 'ngx-toastr';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
