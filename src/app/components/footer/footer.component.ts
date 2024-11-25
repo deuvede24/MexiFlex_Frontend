5cefa40ecec256b40ce1855425e477d75c801e84
deuvede24 <cdaniela.vd@gmail.com>
2024-11-25 16:37:22 +0100

    footer changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  activeModal: string | null = null;
+
+
+  showModal(modalId: string, event: Event) {
+    event.preventDefault();
+    this.activeModal = modalId;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal() {
+    this.activeModal = null;
+    document.body.style.overflow = 'auto';
+  }
 
 }
