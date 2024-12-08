bc3def907f429d74eac8d20bd8107947ebf697c0
deuvede24 <cdaniela.vd@gmail.com>
2024-12-08 22:47:27 +0100

    better UX/UI modals & text

--- src/main.ts
+++ src/main.ts
@@ -20,29 +20,6 @@ bootstrapApplication(AppComponent, {
   ]
 });
 
-/*import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { provideForms } from '@angular/forms';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Polyfill para `process` (solo si es estrictamente necesario)
-(window as any).process = {
-  env: { DEBUG: undefined },
-};
-
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    provideHttpClient(),
-    provideRouter(appConfig.routes!),  // Asegúrate de que `routes` esté definido en `appConfig`
-    provideForms(),
-    ...appConfig.providers!,
-    importProvidersFrom(BrowserAnimationsModule), // Animaciones
-  ],
-});*/
 
 
